b0d8451de11acb481775643a8d8ef251de708508
Nicholas Haley <nicholaswilliamhaley@gmail.com>
2014-11-08 15:15:48 -0500

    Added associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ActiveRecord::Base
 	has_secure_password	# automatically adds attr_accessor :password, :password_confirmation into the model 
 	# This creates a form that accepts values from the user and has_secure_password will take those values and 
 	# encrypt them into the password_digest field on the users table
+	has_many :reviews
+	has_many :products, through: :reviews
 end
